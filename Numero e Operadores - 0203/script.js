9ef4ef7ea337bb65572bf10d3940c3786229130e
andrecmatz <andrecmatzumura@gmail.com>
2023-10-05 15:52:02 -0300

    Numeros e operadores - 0203

--- Numero e Operadores - 0203/script.js
+++ Numero e Operadores - 0203/script.js
@@ -1,4 +1,4 @@
-var idade = 18e2;
+// var idade = 18;
 
 console.log(idade);
 
@@ -10,7 +10,7 @@ var multiplicacao = 100 * 2; // 200
 var divisao = 100 / 2; // 50
 var expoente = 2 ** 4; // 16
 var modulo = 14 % 5; // 4
-var total = soma - subtracao;
+// var total = soma - subtracao;
 
 console.log(total);
 
@@ -23,3 +23,69 @@ var divisao = 'Comprei 10' / 2; // NaN (Not a Number)
 
 var testeNaN = 'Isso e 10';
 console.log(isNaN(testeNaN));
+
+// Operadores aritmeticos binarios
+
+var total1 = 20 + 5 * 2; // 30 
+var total2 = (20 + 5) * 2; // 50
+// A ordem importa. O sistema segue a ordem de importancia matematica.
+var total3 = 20 / 2 * 5; // 50
+var total4 = 10 + 10 * 2 + 20 / 2; // 40
+
+// Operadores aritmeticos unarios
+
+var x = 5;
+
+// Caso o ++ esteja na frente, a soma so sera retornada no proximo console.log
+// Caso esteja atras (++x), a soma sera retornada no exato momento em que aparecer no console.
+// Em resumo, o ++x e o mesmo que x = x + 1
+// Tambem existem os decrementos, trocando o sinal de + por -
+
+console.log(x);
+console.log(++x);
+
+var idade = '18';
+var somaIdade = 5;
+
+console.log(+idade + somaIdade); // Transformamos a string em numero com o sinal de + | Isso tambem pode ser realizado adicionando o + em frente a string na var idade (var idade = + '18';)
+console.log(+idade + -somaIdade); 
+
+// Exercicios
+
+// Qual o resultado da seguinte expressao?
+
+var total = 10 + 5 * 2 / 2 + 20; // 35
+
+console.log(total);
+
+// Crie duas expressoes que retornem NaN 
+
+var firstNaN = '18s' / 2;
+var secondNaN = 'Isso e um NaN' - 2;
+
+console.log(isNaN(firstNaN));
+console.log(isNaN(secondNaN));
+
+// Somar a string '200' com o numero 50 e retornar 250
+
+var somaString = +'200' + 50;
+
+console.log(somaString);
+
+// Incremente o numero 5 ao resultado acima e retorne o seu valor incrementado
+
+var x = 5;
+console.log(++x);
+
+// Como dividir o peso por 2?
+
+// var numero = '80';
+// var unidade = 'kg';
+// var peso = numero + unidade; // 80kg
+// var pesoPorDois = peso / 2; // NaN (Not a number)
+
+var numero = +'80' / 2
+var unidade = 'kg'
+var peso = numero + unidade;
+
+console.log(peso);
