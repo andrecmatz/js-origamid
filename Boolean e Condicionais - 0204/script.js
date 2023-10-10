ff38ceae45aa0af62364f1f9bd1e7fcb39fbee23
andrecmatz <andrecmatzumura@gmail.com>
2023-10-10 13:02:14 -0300

    0204 - Boolean e condicionais

--- Boolean e Condicionais - 0204/script.js
+++ Boolean e Condicionais - 0204/script.js
@@ -80,3 +80,111 @@ if(!possuiGraduacao) {
 var x = 10;
 
 console.log(x === 10);
+
+// Operadores Logicos && (e)
+
+true && true; // true
+true && false; // false
+false && true; // false
+'Gato' && 'Cao'; // 'Cao'
+(5 - 5) && (5 + 5); // 0
+'Gato' && false; // false
+(5 >= 5) && (3 < 6); // true
+
+// Se ambos os valores forem true, ele ira retornar o ultimo valor verificado.
+// Se algum valor for false, ele ira retornar o mesmo e nao ira  continuar a verificar o restante
+
+var condicional = (5 - 5) && (5 + 5);
+
+if(condicional) {
+    console.log('Verdadeiro');
+} else {
+    console.log('Falso');
+}
+// Retorna falso pois a primeira conta tem resultado 0, que e um valor falso.
+
+// Operadores Logicos || (ou)
+
+true || true; // true
+true || false; // true
+false || true; // true
+'Gato' || 'Cao'; // 'Gato'
+(5 - 5) || (5 + 5); // 10
+'Gato' || false; // Gato
+(5 >= 5) || (3 < 6); // true
+
+var condicional2 = (5 - 5) || (5 + 5) || (10 - 2);
+console.log(condicional2);
+// Retorna 10 pois e o primeiro valor true encontrado.
+
+// Switch
+
+var corFavorita = 'Verde';
+
+switch(corFavorita) {
+    case 'Azul':
+        console.log('Olhe para o ceu.');
+        break;
+    case 'Amarelo':
+        console.log('Olhe para o sol.');
+        break;
+    case 'Verde':
+        console.log('Olhe para a floresta.')
+        break;
+    default:
+        console.log('Feche os olhos.');
+}
+
+//  Exercicios
+
+// Verifique se a sua idade é maior do que a de algum parente
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+
+var minhaIdade = 18;
+var idadeMae = 49;
+
+if(minhaIdade > idadeMae) {
+    console.log('E maior.')
+} else if(minhaIdade === idadeMae) {
+    console.log('E igual');
+} else {
+    console.log('E menor');
+}
+
+// Qual valor é retornado na seguinte expressão?
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
+console.log(expressao);
+
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+var nome = 'Andre';
+var idade = 28;
+var possuiDoutorado = false;
+var empregoFuturo;
+var dinheiroNaConta = 0;
+
+console.log(!!nome, !!idade, !!possuiDoutorado, !!empregoFuturo, !!dinheiroNaConta);
+
+
+// Compare o total de habitantes do Brasil com China (valor em milhões)
+var brasil = 207;
+var china = 1340;
+
+if(brasil > china) {
+    console.log('Brasil tem mais habitantes');
+} else {
+    console.log('Brasil tem menos habitantes');
+}
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') && (5 > 2)) {
+  console.log('Verdadeiro');
+} else {
+  console.log('Falso');
+}
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') || (5 > 2)) {
+  console.log('Gato' && 'Cão');
+} else {
+  console.log('Falso');
+}
